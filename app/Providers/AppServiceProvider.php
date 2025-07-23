ae936f94037bed5d56dec28d765aa41bea14245d
Matteo <mdpproduction@gmail.com>
2025-07-23 12:33:00 +0200

    Fix mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
         Vite::prefetch(concurrency: 3);
 
         JsonResource::withoutWrapping();
+
+        if (config('app.env') != 'local') {
+            URL::forceScheme('https');
+        }        
     }
 }
